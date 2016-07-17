ced8c4a15281ca17eb222051c5a8ed796a65fdd2
Patrick Todd <patrick.a.todd@gmail.com>
2016-07-17 14:23:07 -0400

    modified server to push username and comments section

--- server.py
+++ server.py
@@ -59,32 +59,57 @@ def submit():
 		session['display'] = True
 	return redirect('/')
 
-@app.route('/post', methods=['POST'])
-def post():
-	if len(request.form['message']) > 0:
-		text = request.form['message']
-		query = "INSERT INTO messages (message, created_at, updated_at, users_id) VALUES ( :message, NOW(), NOW(), :users_id )"
-		data = {
-		'message': text,
-		'users_id':session['id']
-		}
-		mysql.query_db(query, data)
-		return redirect('/wall')
-
-	else:
-		return redirect('/wall')
 
 @app.route('/wall', methods=['GET'])
 def messages():
 	print "session id is: ",session['id']
-	
+	name_query = "SELECT first_name FROM users WHERE id = :users"
+	name_data = {
+	'users':session['id']
+	}
+	name_user = mysql.query_db(name_query,name_data)
+	name =  str(name_user[0]['first_name'])
 	name_and_message_query = "SELECT concat_ws(' ',first_name,last_name), users_id, messages.message, date_format(messages.created_at, '%M %D %Y') AS date, messages.created_at FROM users JOIN messages ON messages.users_id = users.id ORDER BY created_at DESC"
 	name_and_message = mysql.query_db(name_and_message_query)
+
+	query_message_id = "SELECT * FROM messages LEFT JOIN users ON users.id = messages.users_id LEFT JOIN comments ON comments.messages_id = messages.users_id"
+	
+
 	for x in range(0,len(name_and_message)):
-		flash('<div class="message"><h4>' +name_and_message[x]["concat_ws(' ',first_name,last_name)"]+ ' - ' +name_and_message[x]['date']+ '</h4><p>' +name_and_message[x]['message']+ '</p><hr></div>')
-	return render_template('wall.html')
+		flash('<div class="message"><h4>' +name_and_message[x]["concat_ws(' ',first_name,last_name)"]+ ' - ' +name_and_message[x]['date']+ '</h4><p>' +name_and_message[x]['message']+ '</p>Post a comment: <br><form action="/comment" method="POST"><input type="text" name="comment" class="textbox"><input type="submit" value="Post a comment" class="btn btn-success"></form><br><hr></div>')
+	return render_template('wall.html', name=name)
 
 
+@app.route('/comment', methods=['POST'])
+def comment():
+	if len(request.form['comment']) > 0:
+		comment = request.form['comment']
+		query_comment = "INSERT INTO comments (comment, created_at, updated_at, users_id, messages_id) VALUES (:comment, NOW(), NOW(), :users_id, :message_id)"
+		data_comment = {
+		'comment':comment,
+		'users_id':session['id'],
+		}
+		mysql.query_db(query_comment, data_comment)
+		return redirect('/wall')
+	else:
+		return redirect('/wall')
+
+
+
+@app.route('/post', methods=['POST'])
+def post():
+	if len(request.form['message']) > 0:
+		post = request.form['message']
+		query_post = "INSERT INTO messages (message, created_at, updated_at, users_id) VALUES ( :message, NOW(), NOW(), :users_id )"
+		data = {
+		'message': post,
+		'users_id':session['id']
+		}
+		mysql.query_db(query_post, data)
+		return redirect('/wall')
+
+	else:
+		return redirect('/wall')
 
 
 
